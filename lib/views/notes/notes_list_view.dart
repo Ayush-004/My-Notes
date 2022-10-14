ebce917a0a50969da88a472538267b56cb78e8d5
Ayush Malik <ayush2003malik@gmail.com>
2022-10-14 18:23:28 +0530

    Update note

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -5,16 +5,18 @@ import 'package:mynotes/utilities/dialogs/delete_dialog.dart';
 
 import '../../services/crud/notes_service.dart';
 
-typedef DeleteNoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(DatabaseNote note);
 
 class NotesListView extends StatelessWidget {
   final List<DatabaseNote> notes;
-  final DeleteNoteCallback onDeleteNote;
+  final NoteCallback onDeleteNote;
+  final NoteCallback onTap;
 
   const NotesListView({
     super.key,
     required this.notes,
     required this.onDeleteNote,
+    required this.onTap,
   });
 
   @override
@@ -24,6 +26,9 @@ class NotesListView extends StatelessWidget {
       itemBuilder: (context, index) {
         final note = notes[index];
         return ListTile(
+          onTap: () {
+            onTap(note);
+          },
           title: Text(
             note.text,
             maxLines: 1,
@@ -37,7 +42,7 @@ class NotesListView extends StatelessWidget {
                 onDeleteNote(note);
               }
             },
-            icon: Icon(Icons.delete),
+            icon: const Icon(Icons.delete),
           ),
         );
       },
